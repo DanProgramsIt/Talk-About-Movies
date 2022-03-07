084c9b226865922d153e4179a30416d0dacd1b97
Daniel Mendez <soliddan211@gmail.com>
2022-03-06 22:55:28 -0500

    added code for signupand.js -- main, login and dashboard.handlebars

---
+++ public/javascript/signup.js
@@ -0,0 +1,30 @@
+async function createAccount(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#signup-username").value.trim();
+  const email = document.querySelector("#signup-email").value.trim();
+  const password = document.querySelector("#signup-password").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      console.log(`The Account for ${username} has been created`);
+      document.location.replace("/login");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", createAccount);
